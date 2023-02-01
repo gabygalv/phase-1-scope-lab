c964ac700c21d640e0888ecaaa12e20331e09af7
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-01-31 17:20:25 -0700

    completed scope lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bobby';
+
+function getCustomerName() {
+    return customerName;
+}
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer () {
+    bestCustomer = 'not bob'; 
+    return bestCustomer;
+}
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'bob';
+    return leastFavoriteCustomer;
+}
